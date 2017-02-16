0a929a6c5df18588fef060206b98a06500788a8e
lceames <lceames@gmail.com>
2017-02-16 09:07:52 -0500

    fix static images bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214171905) do
+ActiveRecord::Schema.define(version: 20170216135850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tracks", force: :cascade do |t|
+    t.string  "title",       null: false
+    t.integer "user_id",     null: false
+    t.text    "description"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
