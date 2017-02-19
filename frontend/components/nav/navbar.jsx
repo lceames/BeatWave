d15a1b5543db9218ee547983baf71accf31045c0
lceames <lceames@gmail.com>
2017-02-19 16:06:00 -0500

    restyle navbar

--- frontend/components/nav/navbar.jsx
+++ frontend/components/nav/navbar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import UserNavSectionContainer from './user_nav_section_container';
-import { hashHistory } from 'react-router';
+import { hashHistory, Link } from 'react-router';
 import { fetchTrack } from '../../actions/track_actions';
 
 export default (props) => {
   window.fetchTrack = fetchTrack;
   let className = props.location === "/" ? "nav-bar nav-splash" : "nav-bar" ;
+  let home = currentUser ? "/stream" : "/";
   return (
     <nav className={className}>
-      <img id="logo" src={window.images.beatWaveLogo}/>
-
-      <button onClick={() => hashHistory.push("/upload")} id="upload">Upload</button>
-      <UserNavSectionContainer />
+      <div className="left-header-section">
+        <button className="logo-background">
+          <img id="logo" src={window.images.beatWaveLogo}/>
+        </button>
+        <Link to={home} className="home">Home</Link>
+      </div>
+      <div className="right-header-section">
+        <button onClick={() => hashHistory.push("/upload")} id="upload">Upload</button>
+        <UserNavSectionContainer />
+      </div>
     </nav>
   )
 };
