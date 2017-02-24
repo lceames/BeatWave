93276744fd14a8e6d69ed2aab05e156d01ff4d9d
lceames <lceames@gmail.com>
2017-02-23 23:07:17 -0500

    add peaks to track model and table

--- app/models/track.rb
+++ app/models/track.rb
@@ -18,14 +18,23 @@
 #
 
 class Track < ApplicationRecord
-  validates :title, :user_id, presence: true
+  validates :title, :user_id, :peaks, presence: true
   belongs_to :user
   has_many :comments
+  before_validation :get_peaks
+  serialize :peaks
+
+  def get_peaks
+    peaks = Dir.chdir('app/models/wav2json/build'){
+      return (`wav2json #{self.audio.url} --channels left -o -`)
+    }
+    debugger
+    self.peaks = peaks["left"]
+  end
 
   has_attached_file :audio, default_url: "life.mp3", s3_protocol: :https
   validates_attachment_content_type :audio, content_type: /\Aaudio\/.*\z/
 
   has_attached_file :image, default_url: "record.png", s3_protocol: :https
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
 end
