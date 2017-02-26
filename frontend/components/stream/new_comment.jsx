f3b48698e53dc3c20018b5090d6bf043bc5c063b
lceames <lceames@gmail.com>
2017-02-26 18:09:30 -0500

    refactor play button new comment form

---
+++ frontend/components/stream/new_comment.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default class newComment extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      body: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let elapsedTime = this.props.currentTrack ? this.props.currentTrack.elapsedTime : 0;
+    let comment = Object.assign({}, this.state);
+    comment["track_id"] = this.props.track.id;
+    comment["elapsed_time"] = elapsedTime;
+    this.props.createComment(comment).then( () => this.setState({body: ""}));
+  }
+
+  handleChange(e) {
+    this.setState({body: e.target.value});
+  }
+
+  render () {
+    return (
+      <div className="new-comment">
+        <img className="new-comment-thumb" src={this.props.currentUser.image}/>
+        <form onSubmit={this.handleSubmit} className="new-comment-form">
+          <input type="text" placeholder="Write a comment" className="comment-text"
+          onChange={this.handleChange} value={this.state.body}
+          />
+        </form>
+      </div>
+    )
+  }
+}
