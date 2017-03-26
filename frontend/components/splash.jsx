c760a7d53268511889f390a65bc0201d903fbefa
lceames <lceames@gmail.com>
2017-03-26 18:38:05 -0400

    add track queue to splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -2,14 +2,55 @@ import React from 'react';
 import { hashHistory } from 'react-router';
 import NavBar from './nav/navbar';
 import StreamContainer from './stream/stream_container';
+import { connect } from 'react-redux';
+import { fetchTracks } from '../actions/track_actions';
+import SplashIndexItem from './splash_index_item';
 
-export default (props) => {
-  return (
-    <div className="splash">
-      <div className="header-section">
-        <NavBar location={props.location.pathname}/>
-        <img id="splash-header" src={window.images.splashHeader}/>
+
+class Splash extends React.Component {
+  componentDidMount() {
+    this.props.fetchTracks("splash");
+  }
+
+  render () {
+    let tracks;
+    if (this.props.tracks.length > 0) {
+      tracks = this.props.tracks.map( track => {
+        return <SplashIndexItem track={track} key={track.id}/>
+      });
+    }
+
+    return (
+      <div className="splash">
+        <div className="header-section">
+          <NavBar location={this.props.location.pathname}/>
+          <img id="splash-header" src={window.images.splashHeader}/>
+          <div className="splash-content">
+            <h2>Hear what's trending on BeatWave</h2>
+          </div>
+        </div>
+        <div className="splash-items">
+          {tracks}
+        </div>
       </div>
-    </div>
-  )
-};
+    )
+  };
+}
+
+
+const mapStateToProps = state => {
+  return {
+    tracks: state.trackQueue.queue
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchTracks: (filter) => dispatch(fetchTracks(filter))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Splash)
---
+++ frontend/components/splash_index_item.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PlayPause from './stream/play_pause';
+import { connect } from 'react-redux';
+import { hashHistory } from 'react-router';
+
+const SplashIndexItem = props => {
+  return (
+    <div className="splash-index-item">
+      <img src={props.track.image} className="track-image" onClick={ () => hashHistory.push(`/${props.track.user_id}/${props.track.id}`)}/>
+      <PlayPause track={props.track} type="splash" currentTrack={props.currentTrack} queue={props.queue}/>
+    </div>
+  );
+};
+
+
+const mapStateToProps = state => {
+  return {
+    tracks: state.trackQueue.queue,
+    currentTrack: state.trackQueue.currentTrack
+  }
+}
+export default connect(
+  mapStateToProps,
+  null
+)(SplashIndexItem);
